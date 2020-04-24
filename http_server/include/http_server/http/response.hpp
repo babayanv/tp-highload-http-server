cbac211a382c22f73dec48e8ae8556d31db45691
Babich <g8h2.babayan@yandex.ru>
2020-04-24 05:35:27 +0300

    implemented http ResponseBuilder

---
+++ http_server/include/http_server/http/response.hpp
@@ -0,0 +1,35 @@
+#ifndef HTTP_RESPONSE_HPP
+#define HTTP_RESPONSE_HPP
+
+#include "http_server/http/http.hpp"
+#include "http_server/http/request.hpp"
+#include "http_server/connection.hpp"
+
+#include <sstream>
+
+
+namespace http {
+
+
+void build_and_send_response(Connection& conn, const Request& request, status::value_type& status);
+
+
+class ResponseBuilder {
+public:
+    ResponseBuilder() = default;
+
+    void operator()(Connection& conn, const Request& request, status::value_type& status);
+
+private:
+    std::ostringstream m_oss;
+
+private:
+    void check_file_availability(const std::string_view& file_path, status::value_type& status);
+    void write_headers(const Request& request, status::value_type& status);
+    void write_file_content(const std::string_view& file_path);
+};
+
+
+} // namespace http
+
+#endif // HTTP_RESPONSE_HPP
---
+++ http_server/src/http/response.cpp
@@ -0,0 +1,74 @@
+#include "http_server/http/response.hpp"
+
+#include <fstream>
+#include <filesystem>
+
+
+namespace http {
+
+
+void build_and_send_response(Connection& conn, const Request& request, status::value_type& status) {
+    ResponseBuilder rb;
+    return rb(conn, request, status);
+}
+
+
+void ResponseBuilder::operator()(Connection& conn, const Request& request, status::value_type& status) {
+    namespace fs = std::filesystem;
+
+    check_file_availability(request.path, status);
+
+    write_headers(request, status);
+
+    if (status == status::S_200_OK && request.method == method::M_GET) {
+        write_file_content(request.path);
+    }
+
+    conn.write_exact(m_oss.str().c_str(), static_cast<size_t>(m_oss.tellp()));
+}
+
+
+void ResponseBuilder::check_file_availability(const std::string_view& file_path, status::value_type& status) {
+    namespace fs = std::filesystem;
+
+    if (status == status::S_200_OK && !fs::exists(file_path)) {
+        if (file_path.ends_with("index.html")) {
+            status = status::S_403_F;
+        }
+        else {
+            status = status::S_404_NF;
+        }
+    }
+}
+
+
+void ResponseBuilder::write_headers(const Request& request, status::value_type& status) {
+    namespace fs = std::filesystem;
+
+    m_oss << request.version << ' ' << status << CRLF
+          << "Server: tp-highload-http" << CRLF
+          << "Date: " << __DATE__ << ' ' << __TIME__ << CRLF
+          << "Connection: Close" << CRLF;
+
+    if (status == status::S_200_OK) {
+        m_oss << "Content-Length: " << fs::file_size(request.path) << CRLF
+              << "Content-Type: " << determine_mime_type(request.path) << CRLF;
+    }
+
+    m_oss << CRLF;
+}
+
+
+void ResponseBuilder::write_file_content(const std::string_view& file_path) {
+    namespace fs = std::filesystem;
+
+    std::ifstream ifs(file_path.data());
+
+    std::string content(fs::file_size(file_path), '\0');
+    long bytes_read = ifs.readsome(content.data(), static_cast<long>(content.size()));
+
+    m_oss.write(content.c_str(), bytes_read);
+}
+
+
+} // namespace http
